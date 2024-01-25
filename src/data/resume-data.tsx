9a7114eab08a3bda66a64317e1874716a892a2f7
arodla <44733423+arodla@users.noreply.github.com>
2024-01-25 17:14:45 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,270 +1,110 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
-
-export const RESUME_DATA = {
-  name: "Antonio Rodríguez Laguna",
-  initials: "ARL",
-  location: "Madrid, Spain",
-  locationLink: "https://www.google.com/maps/place/Madrid",
-  about:
-    "FP&A Manager focused on data driven decisions.",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://arodla.com",
-  contact: {
-    email: "antonio.rodriguez.laguna@gmail.com",
-    tel: "+34627790139",
-    social: [
+    AmbitLogo,
+    BarepapersLogo,
+    BimLogo,
+    CDGOLogo,
+    ClevertechLogo,
+    ConsultlyLogo,
+    EvercastLogo,
+    Howdy,
+    JarockiMeLogo,
+    JojoMobileLogo,
+    Minimal,
+    MobileVikingsLogo,
+    MonitoLogo,
+    NSNLogo,
+    ParabolLogo,
+    TastyCloudLogo,
+    YearProgressLogo,
+  } from "@/images/logos";
+  import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+  
+  export const RESUME_DATA = {
+    name: "Antonio Rodríguez Laguna",
+    initials: "ARL",
+    location: "Madrid, Spain",
+    locationLink: "https://www.google.com/maps/place/Madrid",
+    about:
+      "FP&A Manager focused on data driven decisions.",
+    summary:
+      "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
+    avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+    personalWebsiteUrl: "https://arodla.com",
+    contact: {
+      email: "antonio.rodriguez.laguna@gmail.com",
+      tel: "+34627790139",
+      social: [
+        {
+          name: "GitHub",
+          url: "https://github.com/arodla",
+          icon: GitHubIcon,
+        },
+        {
+          name: "LinkedIn",
+          url: "https://www.linkedin.com/in/arodriguezlaguna/",
+          icon: LinkedInIcon,
+        },
+        {
+          name: "X",
+          url: "https://x.com/arodla90",
+          icon: XIcon,
+        },
+      ],
+    },
+    education: [
       {
-        name: "GitHub",
-        url: "https://github.com/arodla",
-        icon: GitHubIcon,
+        school: "UNIR",
+        degree: "Executive's Master in Financial Management",
+        start: "2021",
+        end: "2022",
+      },
+        {
+        school: "Universidad Pontificia Comillas - ICADE",
+        degree: "Master's Degree in Financial Audit",
+        start: "2013",
+        end: "2015",
       },
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/arodriguezlaguna/",
-        icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/arodla90",
-        icon: XIcon,
+        school: "Universidad Pontificia Comillas - ICADE",
+        degree: "Bachelor's Degree in Business Administration",
+        start: "2008",
+        end: "2013",
       },
     ],
-  },
-  education: [
-    {
-      school: "UNIR",
-      degree: "Executive's Master in Financial Management",
-      start: "2021",
-      end: "2022",
-    },
+    work: [
       {
-      school: "Universidad Pontificia Comillas - ICADE",
-      degree: "Master's Degree in Financial Audit",
-      start: "2013",
-      end: "2015",
-    },
-    {
-      school: "Universidad Pontificia Comillas - ICADE",
-      degree: "Bachelor's Degree in Business Administration",
-      start: "2008",
-      end: "2013",
-    },
-  ],
-  work: [
-    {
-      company: "Grupo Proeduca ",
-      link: "https://www.grupoproeduca.com/",
-      badges: ["Remote"],
-      title: "Financial Planning & Analysys Manager",
-      logo: ParabolLogo,
-      start: "2018",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Deloitte",
-      link: "https://www2.deloitte.com/es/es.html",
-      badges: ["Remote"],
-      title: "Experienced Senior Auditor",
-      logo: ClevertechLogo,
-      start: "2013",
-      end: "2018",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    
-  ],
-  skills: [
-    "Financial Modelling",
-    "Budgeting",
-    "Forecasting",
-    "Accounting",
-    "Microsoft Excel",
-    "PowerBI",
-    "Python",
-  ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        company: "Grupo Proeduca ",
+        link: "https://www.grupoproeduca.com/",
+        badges: ["Remote"],
+        title: "Financial Planning & Analysis Manager",
+        logo: ParabolLogo,
+        start: "2018",
+        end: "2024",
+        description:
+          "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
       },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+      {
+        company: "Deloitte",
+        link: "https://www2.deloitte.com/es/es.html",
+        badges: ["Remote"],
+        title: "Experienced Senior Auditor",
+        logo: ClevertechLogo,
+        start: "2013",
+        end: "2018",
+        description:
+          "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
       },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
+      
+    ],
+    skills: [
+      "Financial Modelling",
+      "Budgeting",
+      "Forecasting",
+      "Accounting",
+      "Microsoft Excel",
+      "PowerBI",
+      "Python",
+    ],
 
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
-} as const;
+  } as const;
+  
